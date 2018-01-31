1e8c214b3785ef0c90b4449d06db9092fece9ff4
BSvetlana <svetlanabisacki@gmail.com>
2018-01-31 20:18:24 +0100

    Second change

--- single-post.php
+++ single-post.php
@@ -1,7 +1,7 @@
 
 
     <div class="blog-post">
-        <h2 class="blog-post-title">Sample blog post</h2>
+        <h2 class="blog-post-title"><a href="">Sample blog post</a></h2>
         <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
 
         <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic
